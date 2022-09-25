61dc6af8bfce4d8a72a14883cc17680de2aaeff9
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-09-25 16:53:32 +0600

    second commit

--- js/home.js
+++ js/home.js
@@ -20,10 +20,32 @@ vehicleData.forEach(data => {
         <p>quantity: ${quantity}</p>
       </div>
       <div class="card-actions flex justify-between">
-      <input type="text" placeholder="Enter quantity" class="input input-bordered input-primary w-[60%]" />
-        <button class="btn btn-primary w-[35%]">Buy Now</button>
+      <input type="text" placeholder="Enter quantity" class="input input-bordered input-primary w-[60%] quantity-input" />
+        <button class="btn btn-primary w-[35%]" disabled>Buy Now</button>
       </div>
     </div>
     `
     parentContainer.appendChild(div)
 })
+
+// event listeners here 
+const quantityFields = document.getElementsByClassName('quantity-input')
+for(const quantity of quantityFields){
+  quantity.addEventListener('keyup',(e)=>{
+    console.log(e.target.value)
+    console.log()
+    if(e.target.value !== ''){
+      e.target.parentNode.children[1].removeAttribute('disabled')
+    }
+    else{
+      e.target.parentNode.children[1].setAttribute('disabled',true)
+    }
+  })
+}
+
+
+
+//navigate to login page
+const navigateToLogin = () =>{
+  location.pathname = '/index.html'
+}
